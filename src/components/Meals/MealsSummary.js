27a6b5a9c81b29b2831b8e2e5ae69a238e0cf7ef
Riuseigi <troyliamjarata@gmail.com>
2023-03-21 10:56:58 +0800

    adding meals component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+  return (
+   <section className={classes.summary}>
+    <h2>Masarap na Pagkain, Ihahatid sayo</h2>
+    <p>
+        Pumili ka nga paborito mong pagkain sa aming pagpipilian na mayroon nakalagay sa aming menu at magenjoy sa masasarap na pagkain ngayong tanghalian at gabihan.
+    </p>
+    <p>
+        Lahat ng aming ulam at pagkain dito ay niluto ng mahusay na mga tagaluto at magaganda ang aming mga sinasangkap. 
+    </p>
+   </section>
+  )
+}
+
+export default MealsSummary
